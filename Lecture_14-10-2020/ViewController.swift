ce8d6729e75240d6468b31e9c4eb8fce74f8b726
Omar Ahmad <superomar121@gmail.com>
2020-10-31 08:37:56 -0700

    Added gesture recognizer to second face

--- Lecture_14-10-2020/ViewController.swift
+++ Lecture_14-10-2020/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateUI()
+        secondUpdateUI()
         
     }
 
@@ -101,15 +102,148 @@ class ViewController: UIViewController {
 
         }
     }
+    @IBAction func secondTap(_ recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .ended {
+            print("ouch")
+            
+            switch secondExpression.mouth {
+            case .Joy:
+                secondExpression.mouth = .Smile
+            case .Smile:
+                secondExpression.mouth = .SlightSmile
+            case .SlightSmile:
+                secondExpression.mouth = .Neutral
+            case .Neutral:
+                secondExpression.mouth = .Frown
+            case .Frown:
+                secondExpression.mouth = .Sad
+            case .Sad:
+                secondExpression.mouth = .Joy
+                
+            }
+            print(secondExpression.mouth)
+        }
+    }
+    @IBAction func secondHoldOnFace(_ recognizer: UILongPressGestureRecognizer) {
+        if recognizer.state == .began {
+               print("begin")
+               
+               
+               switch secondExpression.eyes {
+                case .Open:
+                    secondExpression.eyes = .Closed
+                case .Closed:
+                    secondExpression.eyes = .LeftOpenRightClosed
+                case .LeftOpenRightClosed:
+                    secondExpression.eyes = .LeftClosedRightOpen
+             
+               case .LeftClosedRightOpen:
+                secondExpression.eyes = .Open
+               }
+        
+            print(secondExpression.eyes)
+           }
+    }
+    @IBAction func secondPinchFace(_ recognizer: UIPinchGestureRecognizer) {
+        if recognizer.state == .began {
+            print("I got pinched!")
+            secondExpression.colorPattern = .Yellow
+            secondExpression.eyes = .Open
+            secondExpression.mouth = .Smile
+
+        }
+    }
     @IBOutlet weak var firstFaceView: FaceView! {
         didSet {
             updateUI()
         }
     }
-    var expression = Expression(eyes: .Open, mouth: .Smile, colorPattern: .Ghost) {
+    var expression = Expression(eyes: .Open, mouth: .Smile, colorPattern: .Pumpkin) {
         didSet{
             updateUI() // to update the view every time the model changes
         }
+    }
+    @IBOutlet weak var secondFaceView: FaceView! {
+        didSet {
+            secondUpdateUI()
+        }
+    }
+    var secondExpression = Expression(eyes: .Closed, mouth: .Smile, colorPattern: .Ghost) {
+        didSet{
+            secondUpdateUI() // to update the view every time the model changes
+        }
+    }
+    
+   func secondUpdateUI() {
+            print("I am in SECOND UPDATE UI function")
+            switch secondExpression.eyes {
+            case .Closed :
+                secondFaceView.leftEyeOpen = false
+                secondFaceView.rightEyeOpen = false
+            case .Open :
+                secondFaceView.leftEyeOpen = true
+                secondFaceView.rightEyeOpen = true
+            case .LeftOpenRightClosed :
+                secondFaceView.leftEyeOpen = true
+                secondFaceView.rightEyeOpen = false
+            case .LeftClosedRightOpen :
+                secondFaceView.leftEyeOpen = false
+                secondFaceView.rightEyeOpen = true
+        }
+            switch secondExpression.mouth {
+            case .Neutral :
+                secondFaceView.mouthCurvature = 0
+                secondFaceView.isMouthFilled = false
+            case .Smile :
+                secondFaceView.mouthCurvature = 0.4
+                secondFaceView.isMouthFilled = false
+            case .Joy :
+                secondFaceView.mouthCurvature = 0.6
+                secondFaceView.isMouthFilled = true
+            case .SlightSmile :
+                secondFaceView.mouthCurvature = 0.2
+                secondFaceView.isMouthFilled = false
+            case .Frown:
+                secondFaceView.mouthCurvature = -0.2
+                secondFaceView.isMouthFilled = false
+            case .Sad :
+                secondFaceView.mouthCurvature = -0.4
+                secondFaceView.isMouthFilled = false
+            }
+            
+            switch secondExpression.colorPattern {
+            case .Yellow:
+                secondFaceView.skullColor = UIColor(red: 253/255, green: 213/255, blue: 60/255, alpha: 1.0) // Yellow
+                secondFaceView.eyeColor = UIColor.white
+                secondFaceView.eyeBallColor = UIColor(red: 58/255, green: 46/255, blue: 57/255, alpha: 1.0) // Black Coffee Color
+                secondFaceView.mouthColor = firstFaceView.eyeBallColor
+            case .Robot:
+                secondFaceView.skullColor = UIColor(red: 14/255, green: 121/255, blue: 178/255, alpha: 1.0) // Alien Green
+                secondFaceView.eyeColor = UIColor(red: 58/255, green: 46/255, blue: 57/255, alpha: 0.5) // Black Coffee Color
+                secondFaceView.eyeBallColor = UIColor(red: 35/255, green: 240/255, blue: 199/255, alpha: 1.0) // White Eyeball
+                secondFaceView.mouthColor = secondFaceView.eyeBallColor
+
+            case .Pumpkin:
+                secondFaceView.skullColor = UIColor(red: 254/255, green: 127/255, blue: 45/255, alpha: 1.0) // Alien Green
+                secondFaceView.eyeColor = UIColor(red: 81/255, green: 33/255, blue: 0/255, alpha: 1.0) // Black Coffee Color
+                secondFaceView.eyeBallColor = secondFaceView.eyeColor
+                secondFaceView.mouthColor = secondFaceView.eyeBallColor
+            case .Frog:
+                secondFaceView.skullColor = UIColor(red: 7/255, green: 75/255, blue: 26/255, alpha: 1.0) // Yellow
+                secondFaceView.eyeColor = UIColor.white
+                secondFaceView.eyeBallColor = UIColor.black
+                secondFaceView.mouthColor = UIColor.systemRed
+            case .Ghost: //pink-red color
+                secondFaceView.skullColor = UIColor(red: 251/255, green: 254/255, blue: 249/255, alpha: 1) // Yellow
+                secondFaceView.eyeColor = UIColor.white
+                secondFaceView.eyeBallColor = UIColor(red: 54/255, green: 17/255, blue: 52/255, alpha: 1.0) // Black Coffee Color
+                secondFaceView.mouthColor = secondFaceView.eyeBallColor
+            }
+            
+            
+            
+            
+        
     }
     
     func updateUI() {
